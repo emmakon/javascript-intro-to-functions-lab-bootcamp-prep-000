ca6c161750128f991cd166ca08972109c92ea97e
Emma Konrad <emma.konrad92@gmail.com>
2017-09-08 22:18:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,9 +17,7 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string.toLowerCase())
   return `I can't hear you!` }
-  else if {
-    var uppercase = 'HELLO!'
-  (string.toUpperCase())
+  else if  (string.toUpperCase()){
   return `YES INDEED!` }
   else {
     return `I love you, too.`
