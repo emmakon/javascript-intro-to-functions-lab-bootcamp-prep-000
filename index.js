a69f990c78d9cfd9bfa08667a4c10871beeddf30
Emma Konrad <emma.konrad92@gmail.com>
2017-09-08 22:42:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-var uppercase = "HELLO!"
-
-uppercase.toUpperCase() === uppercase // true
-
-var lowercase = 'hello!'
-
-lowercase.toLowerCase() === lowercase // true
-
-var mixedCase = 'Hi there!'
-
-mixedCase.toLowerCase() === mixedCase // false
-
-mixedCase.toUpperCase() === mixedCase // false
-
 function shout(string) {
   return string.toUpperCase()
 }
@@ -27,19 +13,3 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
-
-function sayHiToGrandma(string) {
-  if (string === lowercase) {
-    return `I can't hear you!`
-  }
-else if (string === uppercase) {
-    return `YES INDEED!`
-  }
-  else {
-    return `I love you, too.`
-  }
-}
-
-sayHiToGrandma(I LOVE YOU!)
-sayHiToGrandma(i love you!)
-sayHiToGrandma(I love you, Grandma!)
